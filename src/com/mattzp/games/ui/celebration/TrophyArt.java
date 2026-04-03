a864d5385a625d67f92fab58aecf7b0236112da3
Matthew Wilson <matt_zp@icloud.com>
2026-04-03 10:48:18 +0200

    Refractor TrophyArt with helper methods

--- src/com/mattzp/games/ui/celebration/TrophyArt.java
+++ src/com/mattzp/games/ui/celebration/TrophyArt.java
@@ -3,91 +3,48 @@ package com.mattzp.games.ui.celebration;
 public class TrophyArt {
 
     public static void main(String[] args) {
-
-        trophyIcon(26);
+        trophyIcon();
     }
 
-
-    private static void trophyIcon(int height) {
-
-        int width = height;
-
-        // Head coordinates
-        int trophyHeadHeightStart = (height * 2 / 10) + 1;
-        int trophyHeadHeightEnd = (height * 4 / 10);
-        int headHeight = (trophyHeadHeightEnd - trophyHeadHeightStart) + 1;
-
-        int trophyHeadWidthStart = (width * 2 / 10) + 1;
-        int trophyHeadWidthEnd = (width - trophyHeadWidthStart) + 1;
-        int headWidth = (trophyHeadWidthEnd - trophyHeadWidthStart) + 1;
-
-        // Neck coordinates
-        int trophyNeckHeightStart = (trophyHeadHeightEnd) + 1;
-        int trophyNeckHeightEnd = trophyNeckHeightStart + (headHeight / 2);
-        int neckHeight = (trophyNeckHeightEnd - trophyNeckHeightStart) + 1;
-
-        int neckWidth = headWidth * 6 / 10;
-        int trophyNeckWidthStart = trophyHeadWidthStart + ((headWidth - neckWidth) / 2);
-        int trophyNeckWidthEnd = trophyHeadWidthEnd - ((headWidth - neckWidth) / 2);
-
-        // Column Head coordinates
-        int columnHeadHeightStart = trophyNeckHeightEnd + 1;
-        int columnHeadHeightEnd = columnHeadHeightStart + (neckHeight / 2);
-
-        int columnHeadWidth = neckWidth * 6 / 10;
-        int columnHeadWidthStart = trophyNeckWidthStart + ((neckWidth - columnHeadWidth) / 2);
-        int columnHeadWidthEnd = trophyNeckWidthEnd - ((neckWidth - columnHeadWidth) / 2);
-
-        // Column Cylinder coordinates
-        int columnCyclinderHeightStart = columnHeadHeightEnd  + 1;
-        int columnCyclinderHeightEnd = columnHeadHeightStart + (neckHeight / 2);
-
-        int columnCylinderWidth = columnHeadWidth * 5 / 10;
-        int columnCylinderWidthStart = trophyNeckWidthStart + ((neckWidth - columnHeadWidth) / 2);
-        int columnCylinderWidthEnd = trophyNeckWidthEnd - ((neckWidth - columnHeadWidth) / 2);
-
-        System.out.println("Head rows: " + trophyHeadHeightStart + " to " + trophyHeadHeightEnd);
-        System.out.println("Head cols: " + trophyHeadWidthStart + " to " + trophyHeadWidthEnd);
-        System.out.println("Head height: " + headHeight);
-        System.out.println("Head width: " + headWidth);
-        System.out.println();
-        System.out.println("Neck width: " + neckWidth);
-        System.out.println("Neck rows: " + trophyNeckHeightStart + " to " + trophyNeckHeightEnd);
-        System.out.println("Neck cols: " + trophyNeckWidthStart + " to " + trophyNeckWidthEnd);
-        System.out.println();
-        System.out.println();
-
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= width; j++) {
-
-                // Print Border
-                if ((i == 1 || j == 1) || (i == height || j == width)) {
-                    System.out.print(" * ");
-
-                    //  Print Trophy
-                } else if (
-                    // Trophy Head
-                        (i >= trophyHeadHeightStart && i <= trophyHeadHeightEnd &&
-                                j >= trophyHeadWidthStart && j <= trophyHeadWidthEnd) ||
-                                // Print Trophy Neck
-                                (i >= trophyNeckHeightStart && i <= trophyNeckHeightEnd &&
-                                        j >= trophyNeckWidthStart && j <= trophyNeckWidthEnd) ||
-                                // Print Column Head
-                                (i >= columnHeadHeightStart && i <= columnHeadHeightEnd &&
-                                        j >= columnHeadWidthStart && j <= columnHeadWidthEnd)
-                ) {
-                    System.out.print(" O ");
-
-                } else {
-                    System.out.print("   ");
-                }
-
-            }
-
-            System.out.println();
+    public static void trophyIcon() {
+        for (int i = 0; i < 14; i++) {
+            System.out.println(buildRow(i));
         }
     }
+
+    public static String buildRow(int i) {
+        String row = "";
+
+        for (int j = 0; j < 14; j++) {
+            row += getCell(i, j);
+        }
+
+        return row;
+    }
+
+    public static String getCell(int i, int j) {
+        if (isBorder(i, j)) {
+            return " * ";
+        } else if (isTrophy(i, j)) {
+            return " 0 ";
+        } else {
+            return "   ";
+        }
+    }
+
+    public static boolean isBorder(int i, int j) {
+        return i == 0 || j == 0 || i == 13 || j == 13;
+    }
+
+    public static boolean isTrophy(int i, int j) {
+        return
+                (i == 2 && j >= 2 && j <= 11) ||
+                        (i == 3 && j >= 2 && j <= 11) ||
+                        (i == 4 && j >= 3 && j <= 10) ||
+                        (i == 5 && j >= 4 && j <= 9) ||
+                        (i >= 6 && i <= 9 && j >= 6 && j <= 7) ||
+                        (i == 10 && j >= 5 && j <= 8) ||
+                        (i == 11 && j >= 3 && j <= 10);
+    }
 }
-
-
 
